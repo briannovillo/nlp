279add3fe81205eb177a24e71aca101db53befa4
briannovillo <brian@briannovillo.com.ar>
2019-02-25 10:50:36 -0300

    Remove useless plots

---
+++ file.py
@@ -0,0 +1,12 @@
+import os
+
+def read_file(FILENAME):
+    with open(FILENAME, 'rb') as f:
+        file = f.read()
+    return file
+
+def delete_file(FILENAME):
+    return os.remove(FILENAME)
+
+def execute_cmd(COMMAND):
+    os.system(COMMAND)
--- main.py
+++ main.py
@@ -1,37 +1,28 @@
 import librosa
 import librosa.display
-import matplotlib.pyplot as plt
 from dtw import dtw
 from numpy.linalg import norm
+from file import read_file, delete_file, execute_cmd
 
-def makeSubtitleLineTime(fromFile, toFile):
-    #Loading audio files
-    y1, sr1 = librosa.load(fromFile)
-    y2, sr2 = librosa.load(toFile)
+def compareWordWithDictionary(word):
+    #Loading audio file
+    y1, sr1 = librosa.load("./raw/woman/"+word+".mp3")
 
-    #Showing multiple plots using subplot
-    plt.subplot(1, 2, 1)
-    mfcc1 = librosa.feature.mfcc(y1,sr1)   #Computing MFCC values
-    librosa.display.specshow(mfcc1)
+    with open('./dictionary.txt') as f:
+        for line in f.read().splitlines():
+            #execute_cmd('sh ./say.sh '+line)
 
-    plt.subplot(1, 2, 2)
-    mfcc2 = librosa.feature.mfcc(y2, sr2)
-    librosa.display.specshow(mfcc2)
+            y2, sr2 = librosa.load("./raw/man/"+ line +".mp3")
+            #delete_file("./normalized/"+ line +".wav")
 
-    dist, cost, acc_cost, path = dtw(mfcc1.T, mfcc2.T, dist=lambda x, y: norm(x - y, ord=1))
-    print("The normalized distance between the two : ", fromFile, toFile, dist)   # 0 for similar audios
+            #Computing MFCC values
+            mfcc1 = librosa.feature.mfcc(y1,sr1)
+            librosa.display.specshow(mfcc1)
 
-    plt.imshow(cost.T, origin='lower', cmap=plt.get_cmap('gray'), interpolation='nearest')
-    plt.plot(path[0], path[1], 'w')   #creating plot for DTW
+            mfcc2 = librosa.feature.mfcc(y2, sr2)
+            librosa.display.specshow(mfcc2)
 
-    plt.show()  #To display the plots graphically
+            dist, cost, acc_cost, path = dtw(mfcc1.T, mfcc2.T, dist=lambda x, y: norm(x - y, ord=1))
+            print("The normalized distance between the two : ", word, line, dist)   # 0 for similar audios
 
-makeSubtitleLineTime('./raw/woman/harina.mp3', './normalized/harina.wav')
-makeSubtitleLineTime('./raw/woman/harina.mp3', './normalized/marina.wav')
-makeSubtitleLineTime('./raw/woman/harina.mp3', './normalized/manzana.wav')
-makeSubtitleLineTime('./raw/woman/harina.mp3', './normalized/banana.wav')
-
-makeSubtitleLineTime('./raw/man/harina.mp3', './normalized/harina.wav')
-makeSubtitleLineTime('./raw/man/harina.mp3', './normalized/marina.wav')
-makeSubtitleLineTime('./raw/man/harina.mp3', './normalized/manzana.wav')
-makeSubtitleLineTime('./raw/man/harina.mp3', './normalized/banana.wav')
+compareWordWithDictionary('banana')
